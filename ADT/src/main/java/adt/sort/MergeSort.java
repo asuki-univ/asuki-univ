8b7b96a66c559d92c202ccf991349ed0f98efe38
TomohikoK <Kinebuchi.Tomohiko@gmail.com>
2013-05-27 23:13:36 +0900

    add comment

--- ADT/src/main/java/adt/sort/MergeSort.java
+++ ADT/src/main/java/adt/sort/MergeSort.java
@@ -8,12 +8,17 @@ public class MergeSort implements Sorter {
        sortInner(x, 0, x.length);
     }
     
+    // x のうち、left から right の間をソート
     private void sortInner(int[] x, int left, int right) {
+        // 配列の大きさが 1 以下であればソート済み。
         if (right - left <= 1) { return; }
         
-        int mid = (left + right) / 2;
+        // 配列を left ~ mid, mid ~ right の２つに分けてマージソート
+        int mid = left + (right - left) / 2;
         sortInner(x, left, mid);
         sortInner(x, mid, right);
+
+        // 配列をマージ
         merge(x, left, mid, right);
     }
     
@@ -25,6 +30,7 @@ public class MergeSort implements Sorter {
         int il = left;
         int ir = mid;
         
+        // ２つの配列から、小さい方を選ぶ。同じであれば１つめの配列から選ぶ。
         while (il < mid && ir < right) {
             if (x[il] < x[ir]) {
                 work[iw++] = x[il++];
@@ -33,6 +39,7 @@ public class MergeSort implements Sorter {
             }
         }
         
+        // 選ばれてない配列の要素を一時領域に入れる
         while (il < mid) {
             work[iw++] = x[il++];
         }
@@ -40,6 +47,7 @@ public class MergeSort implements Sorter {
             work[iw++] = x[ir++];
         }
         
+        // 一時領域からコピーし直す。
         System.arraycopy(work, 0, x, left, n);
     }
 }
